7d2d0fbb8b8b4fd961da012b55682042fd7c1537
ggddessgxh <1249440711@qq.com>
2019-01-21 14:34:12 +0800

    ...

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 for a in range(1,5):
     for b in range(1,5):
         for c in range(1,5):
-            for d in range(1,5):
-                if a!=b and a!=c and a!=d and b!=c and b!=d and c!=d:
-                    m=a*1000+b*100+c*10+d
+            if a!=b and a!=c and b!=c :
+                    m=a*100+b*10+c
                     print(m)
